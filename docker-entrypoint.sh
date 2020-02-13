6dda7d40b5920c0dab0d595d9ed852930e1e5031
Aleksandr Danshyn <dalazx@gmail.com>
2020-02-13 16:16:44 +0200

    Addressed comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@
 screen -T xterm-256color -Sdm neuro
 screen -S neuro -p 0 -X stuff "neuro^M"
 
-${MAX_RUN_TIME:=1d}
-exec "timeout" "$MAX_RUN_TIME" "$@"
+MAX_RUN_TIME="${MAX_RUN_TIME:-"1d"}"
+exec timeout "$MAX_RUN_TIME" "$@"
