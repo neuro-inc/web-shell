166057ac0d8adea847b4cd3f950f33f111fdcd1d
Aleksandr Danshyn <dalazx@gmail.com>
2020-02-17 12:37:31 +0200

    Fixed the mouse/touchpad scroll (#7)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "defscrollback 10000" >> ~/.screenrc
+echo "termcapinfo xterm* ti@:te@" >> ~/.screenrc
 screen -T xterm-256color -Sdm neuro
 screen -S neuro -p 0 -X stuff "cat /opt/neuro/web-shell/readme^M"
 
