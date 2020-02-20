7f84c114b2faeffed71280d65940540d0a368ec8
Aleksandr Danshyn <dalazx@gmail.com>
2020-02-20 23:14:24 +0200

    root working dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 echo "defscrollback 10000" >> ~/.screenrc
 echo "termcapinfo xterm* ti@:te@" >> ~/.screenrc
 screen -T xterm-256color -Sdm neuro
-screen -S neuro -p 0 -X stuff "cat /opt/neuro/web-shell/readme^M"
+screen -S neuro -p 0 -X stuff "cd ~; cat /opt/neuro/web-shell/readme^M"
 
 MAX_RUN_TIME="${MAX_RUN_TIME:-"1d"}"
 exec timeout "$MAX_RUN_TIME" "$@"
