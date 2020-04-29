66c240e78b27f54094db864fcfb0ca82cd77ebe1
Aleksandr Danshyn <dalazx@gmail.com>
2020-04-29 12:19:55 +0300

    Exposed WORKDIR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 echo "defscrollback 10000" >> ~/.screenrc
 echo "termcapinfo xterm* ti@:te@" >> ~/.screenrc
 screen -T xterm-256color -Sdm neuro
-screen -S neuro -p 0 -X stuff "cd ~; cat /opt/neuro/web-shell/readme^M"
+screen -S neuro -p 0 -X stuff "cd $WORKDIR; cat /opt/neuro/web-shell/readme^M"
 
 exec "$@"
