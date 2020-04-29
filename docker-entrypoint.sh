18cea44654fa3276887fe519a9451da9b5aa0593
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-04-29 11:54:14 +0300

    Remove job env var-based timeout (#12)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,4 @@ echo "termcapinfo xterm* ti@:te@" >> ~/.screenrc
 screen -T xterm-256color -Sdm neuro
 screen -S neuro -p 0 -X stuff "cd ~; cat /opt/neuro/web-shell/readme^M"
 
-MAX_RUN_TIME="${MAX_RUN_TIME:-"1d"}"
-exec timeout "$MAX_RUN_TIME" "$@"
+exec "$@"
